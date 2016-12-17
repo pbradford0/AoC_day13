0124f028e0bd0f5970aa56b84c6fa0755f2e9a40
pbradford0 <phil.w.bradford@gmail.com>
2016-12-16 22:44:38 -0500

    start

--- day13.py
+++ day13.py
@@ -1,16 +1,21 @@
 #Author: Phil Bradford
 #Solution for http://adventofcode.com/2015/day/13
 
+import sys
+
+def happy_max(filename):
+  return 0
+
 def main():
   if len(sys.argv) != 2:
     print 'Please specify an input file'
     sys.exit(1)
 
-  #a = elf_math(sys.argv[1])
-  #print "Elf math: " + str(a)
+  a = happy_max(sys.argv[1])
+  print "Maximum Happiness: " + str(a)
   
-  b = red_math(sys.argv[1])
-  print "Elf math: " + str(b)
+  #b = red_math(sys.argv[1])
+  #print "Elf math: " + str(b)
 
 if __name__ == '__main__':
   main()
